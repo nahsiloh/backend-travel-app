1ce524d1d65c174a4cc5058235a6036fa58b4d6a
nahsiloh <lishan1512@gmail.com>
2019-10-16 17:20:39 +0800

    installed  cors

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("./db");
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 const trips = require("./routes/trips");
 app.use("/trips", trips);
