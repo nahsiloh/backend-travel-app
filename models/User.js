db3f4e5491fcb9c65e081a461ace5328bc9b179f
nahsiloh <lishan1512@gmail.com>
2019-11-26 15:44:03 +0800

    add email in user Schema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const MyObjectId = mongoose.Types.ObjectId;
+const Schema = mongoose.Schema;
+require("./Trip");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, index: true, unique: true },
-  password: { type: String, required: true, minlength: 8 },
-  trip: [MyObjectId]
+const userSchema = Schema({
+  // _id: Schema.Types.ObjectId,
+  username: { type: String, required: true },
+  email: { type: String, required: true, index: true, unique: true },
+  password: { type: String, required: true, minlength: 8 }
+  // trips: [{ type: Schema.Types.ObjectId, ref: "Trip" }]
 });
 
 const User = mongoose.model("User", userSchema);
